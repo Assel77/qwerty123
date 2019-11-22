c1ca3ead7842c72045080457dcc93417c505a377
Assel77 <57893025+Assel77@users.noreply.github.com>
2019-11-22 22:23:56 +0500

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+  double x,y;
+  cin>>x;
+  y=((abs(x-5)-sin(x))/3)+((sqrt(pow(x,2)+2014))*cos(2*x))-3;
+  cout<<y<<endl;
+  return 0;
+}
